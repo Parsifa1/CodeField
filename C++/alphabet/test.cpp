f5199837d9b5da18cc9e55f767d7962ea10c7b35
Parsifa1 <2746763180@qq.com>
2023-01-31 14:14:58 +0800

    尝试使用类

---
+++ C++/alphabet/test.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Date {
+   public:
+    Date(int year, int month, int day);
+    Date(const Date& d)  // 拷贝构造
+    {
+        year = d.year;
+        month = d.month;
+        day = d.day;
+    }
+    void PrintDate()  // 打印日期
+    {
+        cout << this->year << "-" << this->month << "-" << this->day << endl;
+    }
+
+   private:
+    int year;
+    int month;
+    int day;
+};
+
+Date::Date(int year = 1900, int month = 10, int day = 3)  // 构造函数,全缺省构造
+{
+    Date::year = year;
+    Date::month = month;
+    Date::day = day;
+}
+
+void test()  // 测试代码
+{
+    Date d(2020, 10, 22);
+    d.PrintDate();
+    Date d1;
+    d1.PrintDate();
+}
+int main() {
+    test();
+    return 0;
+}
