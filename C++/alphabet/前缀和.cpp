5d5eee04b0669682737122f8151937f3e822df4e
Parsifa1 <2746763180@qq.com>
2023-02-04 21:34:12 +0800

    go

--- C++/alphabet/前缀和.cpp
+++ C++/alphabet/前缀和.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int a[114],b[114],i,n;
 
+void cal() {
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        b[0] = a[0];
+        b[i] = a[i] + b[i - 1];
+    }
+    for (int j = 0; j < n; j++) {
+        cout << b[j] << " ";
+    }
+}
 int main() {
     cin>>n;
-    for(int i=0;i<n;i++) {
-        cin>>a[i];
-    }
-    for(int i=0;i<n;i++) {
-        b[0]=a[0];
-    b[i]=a[i]+b[i-1];
-    }
-    for(int j=0;j<n;j++) {
-        cout<<b[j]<<" ";
-    }
+    cal();
     return 0;
 }
