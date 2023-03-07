be7012ca2a28707976252df6a9ccc5e27f6f6ad9
Aldric.li <2746763180@qq.com>
2023-03-07 20:14:49 +0800

    Reverse

--- C++/Luogu/P1104.cpp
+++ C++/Luogu/P1104.cpp
@@ -28,3 +28,26 @@ int main() {
     sort(a, a + 101, a[0].cmp);
     for (int i = 0; i < n; i++) a[i].print();
 }
+//C版本：
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// typedef struct student {
+//     char name[20];
+//     int year, month, day;
+// } student;
+
+// int cmp(const void *a, const void *b) {
+//     student *x = (student *) a, *y = (student *) b;
+//     return x->year * 365 + x->month * 30 + x->day >
+//            y->year * 365 + y->month * 30 + y->day;
+// }
+
+// int main() {
+//     student a[102];
+//     int n; scanf("%d", &n);
+//     for (int i = n - 1; i >= 0; i--) scanf("%s %d %d %d", a[i].name, &a[i].year, &a[i].month, &a[i].day);
+//     qsort(a, n, sizeof(student), cmp);
+//     for (int i = 0; i < n; i++) printf("%s\n", a[i].name);
+// }
