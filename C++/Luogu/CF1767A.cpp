0c5d33ee551885ce6b638eee5a391b5aed225602
Aldric.li <2746763180@qq.com>
2023-04-13 00:31:58 +0800

    洛谷练习

---
+++ C++/Luogu/CF1767A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int x1, x2, x3, y1, y2, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    
+    
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+}
