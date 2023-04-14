b9333da64d49468a00190d18563b909bc212845f
Aldric.li <2746763180@qq.com>
2023-04-14 21:25:28 +0800

    洛谷练习

--- C++/Luogu/CF1767A.cpp
+++ C++/Luogu/CF1767A.cpp
@@ -1,18 +1,17 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-void solve() {
-    int x1, x2, x3, y1, y2, y3;
-    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    
-    
-}
-
+// 测试
 int main() {
-    int T;
-    cin >> T;
-    while(T--) {
-        solve();
+    int t;
+    cin >> t;
+    int a, b, c, d, e, f;
+    while (t--) {
+        getchar();
+        cin >> a >> b >> c >> d >> e >> f;
+        if (((a != c) && (a != e) && (c != e)) ||
+            ((b != d) && (b != f) && (d != f)))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
-
 }
