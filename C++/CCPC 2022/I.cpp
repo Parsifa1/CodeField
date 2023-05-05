32f5963df442b552cd1240a738cf2c39c27bfe63
Aldric.li <2746763180@qq.com>
2023-05-05 19:26:31 +0800

    ccpc2022

--- C++/CCPC 2022/I.cpp
+++ C++/CCPC 2022/I.cpp
@@ -1,20 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    int ans = 0;
-    int n;
-    cin >> n;
-    if(n % 2 == 0) {
-        ans = 2 + ((n - 1) * n) / 2;
-    } else {
-        ans = 2 + ((n - 2) * (n + 1)) / 2;
-    }
-    cout << ans << '\n';
-}
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    solve();
-    return 0;
+	int n; cin >> n;
+	cout << (1 <<( n - 1));
 }
