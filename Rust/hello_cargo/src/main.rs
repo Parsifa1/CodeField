9cc71c6908644026ab8e9ab6737682d51e0f6dfc
Aldric.li <2746763180@qq.com>
2023-07-22 23:08:02 +0800

    代码

--- Rust/hello_cargo/src/main.rs
+++ Rust/hello_cargo/src/main.rs
@@ -1,43 +1,42 @@
+use std::io;
 
-use std::io::stdin;
-// use std::cmp::min;
 fn main() {
     let mut input = String::new();
-    stdin().read_line(&mut input).expect("Error read line");
-    let t: i32 = input.trim().parse().expect("Error parse");
-    for _ in 0..t {
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Error read line");
+    io::stdin().read_line(&mut input).expect("failed to read input");
+    let t: i32 = input.trim().parse().expect("failed to parse input");
+    for _i in 0..t {
+        input.clear();
+        io::stdin().read_line(&mut input).expect("failed to read input");
         let mut iter = input.split_whitespace();
-        let n: i64 = iter.next().unwrap().parse().expect("Error parse");
-        let c: i128 = iter.next().unwrap().parse().expect("Error parse");
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Error read line");
+        let n: i64 = iter.next().unwrap().parse().expect("failed to parse input");
+        let c: i128 = iter.next().unwrap().parse().expect("failed to parse input");
+        input.clear();
+        io::stdin().read_line(&mut input).expect("failed to read input");
         let mut iter = input.split_whitespace();
-        let mut s: Vec<i64> = vec![];
-        for _ in 0..n {
-            let num: i64 = iter.next().unwrap().parse().expect("Error parse");
-            s.push(num);
+        let mut s: Vec<i64> = Vec::new();
+        for _j in 0..n {
+            let x: i64 = iter.next().unwrap().parse().expect("failed to parse input");
+            s.push(x);
         }
-        let mut ans: i128 = 0;
-        // 二分答案
         let mut l: i64 = 1;
         let mut r: i64 = (c as f64).sqrt() as i64 + 1;
+        let mut ans: i64 = 0;
         while l <= r {
             let mid: i64 = l + (r - l) / 2;
-            ans = 0;
-            for i in 0..n {
-                ans += (s[i as usize] + 2 * mid).pow(2) as i128;
+            let mut ans128: i128 = 0;
+            for i in 0..n as usize {
+                ans128 += (s[i] + 2 * mid) as i128 * (s[i] + 2 * mid) as i128;
             }
-            if ans < c {
-                l = mid;
-            } else if ans > c {
-                r = mid;
+            if ans128 < c {
+                l = mid + 1;
+                ans = mid;
+            } else if ans128 > c {
+                r = mid - 1;
             } else {
+                ans = mid;
                 break;
             }
         }
-        let ans = l;
         println!("{}", ans);
     }
 }
