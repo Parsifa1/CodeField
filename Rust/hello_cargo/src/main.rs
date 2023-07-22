343c3b9b4eb26ae97cd2e609895b1a1de47f21d7
Aldric.li <2746763180@qq.com>
2023-07-22 22:42:05 +0800

    文件修改

--- Rust/hello_cargo/src/main.rs
+++ Rust/hello_cargo/src/main.rs
@@ -1,3 +1,25 @@
+fn solve() {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    let mut sum = 0;
+    while c < 4000000 {
+        c = a + b;
+        a = b;
+        b = c;
+        if c % 2 == 0 {
+            sum += c;
+        }
+    }
+    println!("{}", sum);
+}
 fn main() {
-    println!("Hello, world!");
+    //输入T，并循环T次solve
+    let mut t = String::new();
+    std::io::stdin().read_line(&mut t).unwrap();
+    let t: i32 = t.trim().parse().unwrap();
+    for _ in 0..t {
+        solve();
+    }
+
 }
