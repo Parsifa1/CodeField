143b5a6f79e3452c42706231d5d2714ce326d13c
Aldric.li <2746763180@qq.com>
2023-07-22 23:04:26 +0800

    代码

--- Rust/hello_cargo/src/main.rs
+++ Rust/hello_cargo/src/main.rs
@@ -1,25 +1,43 @@
-fn solve() {
-    let mut a = 0;
-    let mut b = 1;
-    let mut c = 0;
-    let mut sum = 0;
-    while c < 4000000 {
-        c = a + b;
-        a = b;
-        b = c;
-        if c % 2 == 0 {
-            sum += c;
-        }
-    }
-    println!("{}", sum);
-}
-fn main() {
-    //输入T，并循环T次solve
-    let mut t = String::new();
-    std::io::stdin().read_line(&mut t).unwrap();
-    let t: i32 = t.trim().parse().unwrap();
-    for _ in 0..t {
-        solve();
-    }
 
+use std::io::stdin;
+// use std::cmp::min;
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Error read line");
+    let t: i32 = input.trim().parse().expect("Error parse");
+    for _ in 0..t {
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Error read line");
+        let mut iter = input.split_whitespace();
+        let n: i64 = iter.next().unwrap().parse().expect("Error parse");
+        let c: i128 = iter.next().unwrap().parse().expect("Error parse");
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Error read line");
+        let mut iter = input.split_whitespace();
+        let mut s: Vec<i64> = vec![];
+        for _ in 0..n {
+            let num: i64 = iter.next().unwrap().parse().expect("Error parse");
+            s.push(num);
+        }
+        let mut ans: i128 = 0;
+        // 二分答案
+        let mut l: i64 = 1;
+        let mut r: i64 = (c as f64).sqrt() as i64 + 1;
+        while l <= r {
+            let mid: i64 = l + (r - l) / 2;
+            ans = 0;
+            for i in 0..n {
+                ans += (s[i as usize] + 2 * mid).pow(2) as i128;
+            }
+            if ans < c {
+                l = mid;
+            } else if ans > c {
+                r = mid;
+            } else {
+                break;
+            }
+        }
+        let ans = l;
+        println!("{}", ans);
+    }
 }
