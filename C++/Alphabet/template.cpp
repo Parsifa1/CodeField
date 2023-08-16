8e5f1db83fbf5c9ad6cf7601be122d25742917ef
Aldric.li <2746763180@qq.com>
2023-08-16 16:10:18 +0800

    初始模板修改

--- C++/Alphabet/template.cpp
+++ C++/Alphabet/template.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int N = 1e6 + 5;
-
-
-void init() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-}
+// #define int long long
 
 void solve() {
     
 }
+
+
 signed main() {
-    init();
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
     int T; cin >> T;
-    while(T--) solve();
+    while (T--) solve();
 }
