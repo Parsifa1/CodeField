f9e1d539fa7912d76f9af09adef692a9e40a52e9
Aldric.li <2746763180@qq.com>
2023-09-05 21:20:28 +0800

    链表

--- C++/Alphabet/LinkedList.cpp
+++ C++/Alphabet/LinkedList.cpp
@@ -6,10 +6,8 @@ struct node {
     node* next;
 };
 
-int n;
-node *head, *tail;     //头指针，尾指针
-
-void create() {
+node* create(int n) {
+    node *head, *tail;    //头指针，尾指针
     node* t = new node;
     cin >> t->data, t->next = NULL;
     head = t, tail = t;
@@ -20,23 +18,24 @@ void create() {
         tail->next = p;
         tail = p;
     }
+    return head;
 }
 
-void insert(int x) {
-    node* t = head;
-    while (t != NULL) {
-        if (t->next->data >= x) {
-            node* p = new node;
-            p->data = x;
-            p->next = t->next;
-            t->next = p;
-            break;
-        }
-        t = t->next;
+node* reverse(node* head) {
+    // node *head2 = NULL;
+    auto p = head;
+    auto q = head->next;
+    while(q) {
+        p->next = q->next;
+        q->next = head;
+        head = q;
+        q = p->next;
     }
+    p = NULL;
+    return head;
 }
 
-void print() {
+void print(node* head) {
     node* t = head;
     while (t != NULL) {
         cout << t->data << " ";
@@ -46,9 +45,9 @@ void print() {
 }
 
 int main() {
-    cin >> n;
-    create();
-    print();
-    insert(4);
-    print();
+    int n; cin >> n;
+    auto x = create(n);
+    print(x);
+    print(reverse(x));
+
 }
