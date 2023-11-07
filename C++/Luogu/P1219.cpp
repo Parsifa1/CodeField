50e51a22304872a4bcd6d659196c43c3e458e871
Aldric.li <2746763180@qq.com>
2023-11-07 18:04:15 +0800

    delete wrong file

---
+++ C++/Luogu/P1219.cpp
@@ -0,0 +1,30 @@
+#include <codecvt>
+#include <iostream>
+
+int main() {
+    bool vis[15], tr1[30], tr2[30];
+    int n, ans[15], cnt;
+    std::cin >> n;
+
+    auto dfs = [&](auto && self, int x) {
+        if (x == n + 1) {
+            cnt++;
+            if (cnt <= 3) {
+                for (int i = 1; i <= n; i++) std::cout << ans[i] << " ";
+                std::cout << std::endl;
+            }
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i] && !tr1[x + i] && !tr2[x - i + n]) {
+                vis[i] = tr1[x + i] = tr2[x - i + n] = 1;
+                ans[x] = i;
+                self(self, x + 1);
+                vis[i] = tr1[x + i] = tr2[x - i + n] = 0;
+            }
+        }
+    };
+    dfs(dfs, 1);
+    std::cout << cnt << std::endl;
+    return 0;
+}
